068d443bac0a030954d50cf694da647f94531adc
siulam <m13570009830@163.com>
2026-08-04 13:38:58 +0800

    Add Passwall feed sources
    Added Passwall feed sources to feeds.conf.default.

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+
+# 取消注释或添加 Passwall 软件源
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
