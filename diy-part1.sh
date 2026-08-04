e4de8347a9fb2f3b06e8ef587034317d03f531cc
siulam <m13570009830@163.com>
2026-08-04 15:40:17 +0800

    Update feeds.conf.default to include Passwall
    Uncomment Passwall feed source and add new feed.

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 
 
 # 取消注释或添加 Passwall 软件源
